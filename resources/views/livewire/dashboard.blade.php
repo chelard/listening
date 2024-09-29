ca5493bec2be9dfc58315c2a3f4b329305d84987
Helard Flores <helard@gmail.com>
2024-09-29 17:20:44 -0500

    wireui added, and some components in the form

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -20,6 +20,15 @@ new class extends Component {
     }
 }; ?>
 
-<div>
-    Hola mundo desde Livewire
+<div class="flex items-center justify-center min-h-screen bg-slate-50" >
+    <div class="max-w-lg w-1/2 px-4">
+        <form wire:submit="createListeningParty" class="space-y-6">
+            <x-input wire:model="name" placeholder="Listening Party Name" />
+            <x-datetime-picker wire:model="starTime" placeholder="Start Time" />
+            <x-button primary label="Create Listening Party" />
+        </form>
+
+    </div>
+
+
 </div>
